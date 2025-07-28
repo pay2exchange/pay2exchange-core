45e013d348d142b7f3412827f54d7f588b83c7b5
nullcoset <nullcoset@onet.pl>
2025-07-28 14:20:34 +0200

    Fix debug build symbols

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "This is the script for Developer of application, to build it"
 set -x
-time cmake -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER_LAUNCHER=ccache  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DMANUAL_SUBMODULES=1  .    && time make -j 8
+time cmake -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER_LAUNCHER=ccache  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DMANUAL_SUBMODULES=1  .    && time make -j 6 "$@"

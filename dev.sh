49db585cab1dbe0a9f31286831e090124f56c5eb
entropycoset <nullcoset@onet.pl>
2025-08-22 21:22:54 +0200

    auto detect make jobs based on also MEM, and CPU

--- dev.sh
+++ dev.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "This is the script for Developer of application, to build it"
+jobs=$(./make-get-jobs 1024 110 0 256)
+echo "using $jobs jobs"
 set -x
-time cmake -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER_LAUNCHER=ccache  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DMANUAL_SUBMODULES=1  .    && time make -j 
+time cmake -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER_LAUNCHER=ccache  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DMANUAL_SUBMODULES=1  .    && time make -j ${jobs}

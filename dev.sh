f40877711b259788ebf1fa3c4335f4a461de70cd
entropycoset <nullcoset@onet.pl>
2025-08-22 21:25:49 +0200

    print make jobs

--- dev.sh
+++ dev.sh
@@ -3,4 +3,4 @@ echo "This is the script for Developer of application, to build it"
 jobs=$(./make-get-jobs 1024 110 0 256)
 echo "using $jobs jobs"
 set -x
-time cmake -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER_LAUNCHER=ccache  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DMANUAL_SUBMODULES=1  .    && time make -j ${jobs}
+time cmake -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER_LAUNCHER=ccache  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DMANUAL_SUBMODULES=1  .    && printf "\n\n\nmake with $jobs jobs.\n\n" && time make -j ${jobs}

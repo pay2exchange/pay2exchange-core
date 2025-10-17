0793fb88f1a951d94ec443919134568f5903f33e
entropycoset <nullcoset@onet.pl>
2025-10-17 22:10:14 +0200

    option to ./dev.sh --jobs=N, for testing too

--- dev.sh
+++ dev.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 echo "This is the script for Developer of application, to build it"
 jobs=$(./make-get-jobs 1024 110 0 256)
+
+jobs_arg_forced=false
+for arg in "$@"; do
+  if [[ $arg == --jobs=* ]]; then
+    jobs="${arg#--jobs=}"
+    jobs_arg_forced=true
+  fi
+done
+if $jobs_arg_forced; then
+  echo "Jobs was forced via argument: $jobs"
+fi
+
 echo "using $jobs jobs"
 set -x
 time cmake -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER_LAUNCHER=ccache  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DMANUAL_SUBMODULES=1  .    && printf "\n\n\nmake with $jobs jobs.\n\n" && time make -j ${jobs}
